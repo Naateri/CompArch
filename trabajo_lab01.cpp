e02340a66c67f6a1dc9b3e71a390d9a820549934
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-04 16:40:33 -0500

    avance

---
+++ trabajo_lab01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int booth(int m, int q){ //m = multiplicand, q = multiplier
+	short a, q_1;
+	int n = sizeof(int) * 8; //bits a multiplicar
+	
+}
+
+
+int main(){
+	return 0;
+}
