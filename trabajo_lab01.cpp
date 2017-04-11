fc0cd7618de6e5925e495e88b208877d82fe2a28
Renato Postigo <renato.postigo@ucsp.edu.pe>
2017-04-10 23:46:57 -0500

    hacer lo hecho en division en booth

--- trabajo_lab01.cpp
+++ trabajo_lab01.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <utility>
+#include <string>
 
 using namespace std;
 
+int potenciacion(int b, int p){
+	if (p == 0) return 1; else if (p == 1) return b;	
+	int base = b;	
+	for (int i = 1; i < p; i++)
+		b *= base;
+	return b;
+}
+
+string decToBin(int a){
+	int sz = sizeof(char) * 8, tam, size;
+	string binary;
+	size = sz;
+	for(int i = 0; i < sz; i++){
+		tam = potenciacion(2, size-1);
+		size--;
+		if (a < tam) 
+			binary += '0';
+		else{
+			binary += '1';
+			a -= tam;
+		}
+	}
+	binary += '\0';
+	return binary;
+}
+		
 int booth(int m, int q){ //m = multiplicand, q = multiplier
 	short a = 0, q_1 = 0, q_0;
-	int n = sizeof(int) * 8; //bits a multiplicar
+	int n = sizeof(char) * 8; //bits a multiplicar (8)
 	int count = n;
 	while (count != 0){
 		q_0 = (q & (1 << 0)); // = bit en la pos 0 de q
@@ -13,37 +41,51 @@ int booth(int m, int q){ //m = multiplicand, q = multiplier
 		}else if (q_0 == 1 && q_1 == 0){
 			a -= m;
 		}
-			//} else {
 		a = a >> 1; //shifting a to the right by 1
 		q = q >> 1; //shifting q to the right by 1
 		q_1 = q_1 >> 1; //shifting q_1 to the right by 1
 		count--;
-		//}
 	}
 	return q;
 	
 }
 
-int division(int m, int q){ //m = divisor, q = dividend
-	short a = 0;
-	int n = sizeof(int) * 8; //bits a multiplicar
+pair<int, int> division(int m, int q){ //m = divisor, q = dividend
+	int a = 0;
+	int n = sizeof(char) * 8; //bits a multiplicar (8)
 	int count = n;
-	while (count != 0){
+	short bit;
+	//while (count != 0){
+	for (count; count != 0; count--){
+		cout << "n: " << count << endl;
+		cout << "q: " << decToBin(q) << endl;
+		cout << "a: " << decToBin(a) << endl;
 		a = a << 1; //shifting a to the left by 1
-		q = q << 1; //shifting q to the left by 1 
+		q = q << 1; //shifting q to the left by 1
+		bit = (q >> n) & 1;
+		if (bit == 0) //q = 0x...xx
+			a &= ~(1 << 0);
+		else //q = 1x...xx
+			a |= (1 << 0);
+		cout << "a shifted: " << decToBin(a) << endl;
+		cout << "q shifted: " << decToBin(q) << endl; 
 		a -= m; //a = a - m
+		cout << "a: " << a << endl;
 		//q_0 = (q & (1 << 0)) >> 0; //bit en la pos 0 de q (maskeo)
 		if (a < 0){
-			q |= (0 << 0); //q_0 = 0
-			//q ^= (0 ^ q) & (1 << 0);
+			q &= ~(1 << 0); //q_0 = 0
 			a += m;
 		} else {
-			q |= (1 << 0); //q_0 = 1;
-			//q ^= (-1 ^ q) & (1 << 0);
+			q |= (1 << 0); //q_0 = 1
+			//a += m;
 		}
-		count--;
+		cout << "new q: " << decToBin(q) << endl;
+		//count--;
 	}
-	return q;
+	pair<int, int> results;
+	results.first = q;
+	results.second = a;
+	return results;
 	
 }
 
@@ -55,12 +97,16 @@ int main(){
 		cout << bit;
 		n++;
 	}*/
-	int q, m;
+	int q, m, multi, coc, res;
+	pair<int, int> div;
 	cout << "Ingrese el primer valor: ";
 	cin >> m;
 	cout << "Ingrese el segundo valor: ";
 	cin >> q;
-	cout << "Resultado de la multiplicacion: " << booth(m, q) << endl;
-	cout << "Resultado de la division: " << division(m, q) << endl;
+	div = division(m, q);
+	//cout << "Resultado de la multiplicacion: " << booth(m, q) << endl;
+	coc = div.first; res = div.second;
+	cout << "Resultado de la division: " << coc << endl;
+	cout << "Residuo: " << res << endl;
 	return 0;
 }
